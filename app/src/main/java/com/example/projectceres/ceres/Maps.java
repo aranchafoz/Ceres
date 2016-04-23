383853e5c34c8f22628548fc0f14310d4dfa13e1
aranchafoz <aranchunfli11foz@hotmail.com>
2016-04-23 05:08:43 +0200

    Template for App finished

--- app/src/main/java/com/example/projectceres/ceres/Maps.java
+++ app/src/main/java/com/example/projectceres/ceres/Maps.java
@@ -1,8 +1,12 @@
 package com.example.projectceres.ceres;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 /**
  * Created by aranchafoz on 23/04/16.
@@ -13,5 +17,39 @@ public class Maps extends AppCompatActivity {
         setContentView(R.layout.activity_maps);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_maps);
         setSupportActionBar(toolbar);
+
+        // Habilitas el volver atrás
+        if (getSupportActionBar() != null)
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_maps, menu);
+
+        MenuItem garphicsItem = menu.findItem(R.id.action_graphics);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+
+        switch (item.getItemId()) {
+            case android.R.id.home: // Caso de cuando vuelves atrás
+                this.finish();
+                return true;
+            case R.id.action_graphics:
+                Intent intent = new Intent(this,Graphics.class);
+                startActivity(intent);
+                return true;
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
